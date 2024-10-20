8c8050a420085eafb38bbdffc4f961e5a9560228
sunny <ethsok@protonmail.com>
2024-10-20 14:32:25 -0400

    staring task 3

--- task1.js
+++ task1.js
@@ -6,3 +6,10 @@
 // camelize("-webkit-transition") == 'WebkitTransition';
 
 
+function camelize(str){
+ return str.split('-').map((word, index) => index == 0 ? word: word[0].toUpperCase() + word.slice(1)).join('')
+
+}  
+
+
+console.log(camelize("background-color"))
