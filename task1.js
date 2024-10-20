73c53a67774ee93eb12dc5f848f9eec97f1e10a0
sunny <ethsok@protonmail.com>
2024-10-20 13:49:02 -0400

    array methods

---
+++ task1.js
@@ -0,0 +1,8 @@
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+
