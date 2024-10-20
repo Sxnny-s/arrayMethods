c0d1fb6c81cb79de4c8cc2637efd03e6809f1e6d
sunny <ethsok@protonmail.com>
2024-10-20 15:07:09 -0400

    task 3

--- task3.js
+++ task3.js
@@ -6,8 +6,25 @@
 
 // For instance:
 
-// let arr = [5, 3, 8, 1];
+let x = [5, 3, 8, 9, 1];
 
 // filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
 
 // alert( arr ); // [3, 1]
+
+
+function filterRangeInPlace(arr, a, b){
+    
+    for (let i = 0; i < arr.length; i++){
+
+        let val = arr[i]
+
+        if(val < a || val > b ){
+            arr.splice(i,1)
+            i--
+        }
+    }
+}
+console.log(x)
+filterRangeInPlace(x, 1, 4)
+console.log(x)
